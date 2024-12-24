82fd75cfbe219f012ce6337c906dc94332bfc956
Sanjeev Sharma <shnjeevsharma@gmail.com>
2024-12-24 19:45:41 +0530

    eslint commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import js from '@eslint/js';
-import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
@@ -20,6 +20,14 @@ export default tseslint.config(
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'no-unused-vars': [
+        'warn', // Or "error" if you want to treat unused variables as errors
+        {
+          vars: 'all',
+          args: 'after-used', // Can also be "none" or "all"
+          ignoreRestSiblings: false,
+        },
+      ],
     },
   },
 );
